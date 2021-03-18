18f0083d8e5e3088e7aede883a0f659de9b36e8a
LudovickL <Luberisselud@gmail.com>
2021-03-18 15:57:02 -0400

    message lorsqu'il n'y a pas de signets

--- src/composants/ListeDossiers.jsx
+++ src/composants/ListeDossiers.jsx
@@ -34,9 +34,21 @@ export default function ListeDossiers({utilisateur, etatDossiers}) {
   return (
     <ul className="ListeDossiers">
       {
-        dossiers.map( 
-          dossier =>  <li key={dossier.id}><Dossier {...dossier} /></li>
-        )
+        //S'il y a un dossier:
+        dossiers[0] ?
+          dossiers.map( 
+            dossier =>  <li key={dossier.id}><Dossier {...dossier} /></li>
+          )
+        //Sinon...
+        :
+        <>
+          <div className="aucunDossier">
+          <p>votre liste de dossier est vide</p>
+          <p className="triste">;-(</p>
+          </div>
+          
+        </>
+          
       }
     </ul>
   );
